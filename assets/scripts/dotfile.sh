2ca20f11682218ed59389839aa941d41a74c02a9
Nader Arbabian <Nader.arb72@gmail.com>
2019-04-17 15:39:40 -0400

    add dotfile scripts

---
+++ assets/scripts/dotfile.sh
@@ -0,0 +1,15 @@
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone --bare https://github.com/Nader-gator/dotfiles.git $HOME/.cfg
+function config {
+   /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
+}
+mkdir -p .config-backup
+config checkout
+if [ $? = 0 ]; then
+  echo "Checked out config.";
+  else
+    echo "Backing up pre-existing dot files.";
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+fi;
+config checkout
+config config status.showUntrackedFiles no
