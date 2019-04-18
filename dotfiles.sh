2527d50a44e177796721a377c05c4ed9367ec7df
Nader Arbabian <Nader.arb72@gmail.com>
2019-04-18 19:13:59 -0400

    more descriptive messages

--- dotfiles.sh
+++ dotfiles.sh
@@ -9,9 +9,12 @@ config checkout
 if [ $? = 0 ]; then
   echo "Checked out config.";
   else
-    echo "Backing up pre-existing dot files.";
+    echo -e "\033[1mBacking up pre-existing dot files."
     config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mkdir -p .config-backup/{}
     config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+    echo -e "\033[1mDone backing up"
 fi;
+echo -e "\033[1mChecking out config files"
 config checkout
 config config status.showUntrackedFiles no
+echo -e "\033[1;31m All done \033[0m"
