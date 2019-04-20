19b1305c00a484ead9710737c091d472da4f051f
Nader Arbabian <Nader.arb72@gmail.com>
2019-04-19 20:12:09 -0400

    better uninstall

--- dotfiles.sh
+++ dotfiles.sh
@@ -15,10 +15,7 @@ if [ $? = 0 ]; then
   else
     echo -e "\033[1mBacking up pre-existing dot files."
     config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mkdir -p .config-backup/{}
-    for file in ~/.config-backup/.*
-    do
-      rmdir "$file" > /dev/null 2>&1
-    done
+    find .config-backup -type d -exec rmdir {} + 2>/dev/null
     config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
     echo -e "\033[1mDone backing up"
 fi;
@@ -26,6 +23,11 @@ echo -e "\033[1mChecking out config files"
 config checkout
 config config status.showUntrackedFiles no
 touch .config-backup/uninstall.sh
-echo "mv ~/.config-backup/* ~/; rm -rf ~/.config-backup;rm ~/uninstall.sh" >> ~/.config-backup/uninstall.sh
+echo 'for file in ~/.config-backup/.*
+do rm -rf ~/${file##*/} > /dev/null 2>&1
+done
+cp -a ~/.config-backup/.* ~/ > /dev/null 2>&1
+rm -rf ~/.config-backup
+rm ~/uninstall.sh' >> ~/.config-backup/uninstall.sh
 chmod +x ~/.config-backup/uninstall.sh
 echo -e "\033[1;31m All done \033[0m"
